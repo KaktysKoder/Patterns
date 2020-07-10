ca3335d6437250a64db4bf29e30a8db6c23040ab
Rimuru <kaktysKoder@gmail.com>
2020-07-10 13:02:11 +0400

    init

---
+++ Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal sealed class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello Singleton!");
+        }
+    }
+}
